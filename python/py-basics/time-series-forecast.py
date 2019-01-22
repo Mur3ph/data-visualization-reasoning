3a880eac4fe67a714d9979af2b7937406d7067f0
Mur3ph <paulmurphymainstreet@gmail.com>
2019-01-22 22:50:40 +0000

    show data on plot chart

--- python/py-basics/time-series-forecast.py
+++ python/py-basics/time-series-forecast.py
@@ -7,5 +7,10 @@ from statsmodels.tsa.stattools import acf, pacf
 import statsmodels.tsa.stattools as ts
 from statsmodels.tsa.arima_model import ARIMA
 
+# Tutorial on Time Series with ARIMA: https://machinelearningmastery.com/arima-for-time-series-forecasting-with-python/
+
 variables = pandas.read_csv('data/AirPassengers.csv')
 print(variables.head())
+
+variables.plot()
+plt.show()
