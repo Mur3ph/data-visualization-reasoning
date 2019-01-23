9d1eb7f1441eadf335d88149af99cea5dc6af66e
Mur3ph <paulmurphymainstreet@gmail.com>
2019-01-23 23:16:49 +0000

    learning d3 data joins

--- d3/js-scripts/shapes.js
+++ d3/js-scripts/shapes.js
@@ -31,26 +31,41 @@ function createSquare() {
 function positioningCirclesWithSVG() {
     var spaceCircles = [30, 70, 110];
 
+    var scores = [
+        {
+          "name": "Andy",
+          "score": 30
+        },
+        {
+          "name": "Beth",
+          "score": 70
+        },
+        {
+          "name": "Evelyn",
+          "score": 110
+        }
+      ]
+
     var svgContainer = d3.select("#positioning").append("svg")
         .attr("width", 200)
         .attr("height", 200);
 
     var circles = svgContainer.selectAll("circle")
-        .data(spaceCircles)
+        .data(scores)
         .enter()
         .append("circle");
 
     var circleAttributes = circles
-        .attr("cx", function (d) { return d; })
-        .attr("cy", function (d) { return d; })
+        .attr("cx", function (d) { return d.score; })
+        .attr("cy", function (d) { return d.score; })
         .attr("r", 20)
         .style("fill", function (d) {
             var returnColor;
-            if (d === 30) {
+            if (d.score === 30) {
                 returnColor = "green";
-            } else if (d === 70) {
+            } else if (d.score === 70) {
                 returnColor = "purple";
-            } else if (d === 110) { returnColor = "red"; }
+            } else if (d.score === 110) { returnColor = "red"; }
             return returnColor;
         });
 }
