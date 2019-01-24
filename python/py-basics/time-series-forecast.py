4ff7a81ad55479ef9ba019dda45f85a4ba02c927
Mur3ph <paulmurphymainstreet@gmail.com>
2019-01-24 23:35:14 +0000

    manual input data for displaying new chart

--- python/py-basics/time-series-forecast.py
+++ python/py-basics/time-series-forecast.py
@@ -6,11 +6,22 @@ import math
 from statsmodels.tsa.stattools import acf, pacf
 import statsmodels.tsa.stattools as ts
 from statsmodels.tsa.arima_model import ARIMA
+import pandas as pd
+from pandas.plotting import autocorrelation_plot
+import matplotlib.pyplot as plt
+from pandas import datetime
+import time
 
 # Tutorial on Time Series with ARIMA: https://machinelearningmastery.com/arima-for-time-series-forecasting-with-python/
 
 variables = pandas.read_csv('data/AirPassengers.csv')
 print(variables.head())
 
-variables.plot()
+# variables.plot()
+# plt.show()
+
+dr = pd.date_range(start='1984-01-01', end='1984-12-31')
+df = pd.DataFrame(np.arange(len(dr)), index=dr, columns=["Values"])
+
+autocorrelation_plot(df)
 plt.show()
